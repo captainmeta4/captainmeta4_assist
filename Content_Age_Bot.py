c9aca462ac96e30970c3fcf651383cd55f246487
captainmeta4 <captainmeta4@users.noreply.github.com>
2015-06-04 18:46:58 -0400

    Update Content_Age_Bot.py

--- Content_Age_Bot.py
+++ Content_Age_Bot.py
@@ -148,17 +148,17 @@ class Bot():
             #Remove the submission
             try:
                 submission.remove()
+            
+                #Leave a distinguished message
+                msg=("Thanks for contributing. However, your submission has been automatically removed."+
+                     "\n\nAs per the [subreddit rules](/r/"+submission.subreddit.display_name+"/about/sidebar),"+
+                     "content older than "+str(self.options[submission.subreddit.display_name.lower()])+" days is not allowed.\n\n---\n\n"+
+                     "*I am a bot. Please [Message the Mods](https://www.reddit.com/message/compose?to=/r/"+submission.subreddit.display_name+
+                     "&subject=Question regarding the removal of this submission by /u/"+submission.author.name+
+                     "&message=I have a question regarding the removal of this [submission]("+submission.permalink+") if you feel this was in error.*")
+                submission.add_comment(msg).distinguish()
             except:
                 pass
-            
-            #Leave a distinguished message
-            msg=("Thanks for contributing. However, your submission has been automatically removed."+
-                 "\n\nAs per the [subreddit rules](/r/"+submission.subreddit.display_name+"/about/sidebar),"+
-                 "content older than "+str(self.options[submission.subreddit.display_name.lower()])+" days is not allowed.\n\n---\n\n"+
-                 "*I am a bot. Please [Message the Mods](https://www.reddit.com/message/compose?to=/r/"+submission.subreddit.display_name+
-                 "&subject=Question regarding the removal of this submission by /u/"+submission.author.name+
-                 "&message=I have a question regarding the removal of this [submission]("+submission.permalink+") if you feel this was in error.*")
-            submission.add_comment(msg).distinguish()
 
     def run(self):
         self.initialize()
