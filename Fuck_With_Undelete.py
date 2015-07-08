d87d206f446869dae82d51ce3aa807719c9aa662
captainmeta4 <captainmeta4@users.noreply.github.com>
2015-07-07 20:07:11 -0400

    Update Fuck_With_Undelete.py

--- Fuck_With_Undelete.py
+++ Fuck_With_Undelete.py
@@ -11,19 +11,27 @@ r.login(input("Username: "), input("Password: "))
 #Get a list of subs that the user moderates
 my_moderation=[]
 for subreddit in r.get_my_moderation(limit=1000):
-    my_moderation.append(subreddit.display_name)
+    my_moderation.append(subreddit)
     
 #Load the top 1000 submissions from /r/all/hot and make a listing of submissions that I can remove
 posts = []
+print("Checking /r/all for submissions you can remove")
+i=0
 for submission in r.get_subreddit('all').get_hot(limit=100):
-    if submission.subreddit.display_name in my_moderation:
+    i=i+1
+    print(str(i)+"/100")
+    if submission.subreddit in my_moderation:
         posts.append(submission.fullname)
-        
-print("Found "+str(len(posts))+" usable submissions")
+
+num=len(posts)   
+print("Found "+str(num)+" usable submissions")
 print("Removing posts...")
 
 #Remove all the posts
+i=0
 for submission in r.get_info(thing_id=posts):
+    i=i+1
+    print(str(i)+'/'+str(num))
     submission.remove()
 
 print("posts removed")
@@ -33,7 +41,10 @@ time.sleep(30)
 
 #And put all the posts back up
 print("Restoring posts...")
+i=0
 for submission in r.get_info(thing_id=posts):
+    i=i+1
+    print(str(i)+'/'+str(num))
     submission.approve()
 
 print("posts restored")
